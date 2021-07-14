55bb9142ddd7824ae2c23b6f9ddeeb8499d240fd
Martijn Faber <martijn@fabtronics.nl>
2021-07-14 21:06:47 +0200

    feat(main): add http exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/modules/app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { HttpExceptionFilter } from '@/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get<ConfigService>(ConfigService);
 
   app.enableCors();
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.setGlobalPrefix(config.get('prefix'));
   await app.listen(config.get('port'));
 }
