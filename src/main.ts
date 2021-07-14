0027bc526cb1c9200c01ff021916d99481addb48
Martijn Faber <martijn@fabtronics.nl>
2021-07-14 20:17:37 +0200

    feat(config): add nest config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/modules/app/app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get<ConfigService>(ConfigService);
+
+  app.enableCors();
+  app.setGlobalPrefix(config.get('prefix'));
+  await app.listen(config.get('port'));
 }
 bootstrap();
