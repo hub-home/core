0f6a1d484aeedfe4bae82a7f3149bdc24d102f15
Martijn Faber <martijn@fabtronics.nl>
2021-07-15 14:59:47 +0200

    feat(member): add auth guards

--- src/modules/member/member.controller.ts
+++ src/modules/member/member.controller.ts
@@ -1,26 +1,39 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { MemberService } from './member.service';
+import { JwtAuthGuard } from '@/guards/jwt-auth.guard';
 
 @Controller('members')
 export class MemberController {
   constructor(private memberService: MemberService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   createMember(@Body() body) {
     return this.memberService.createMember(body);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   getMembers() {
     return this.memberService.getMembers();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('/:id')
   getMember(@Param('id') id) {
     return this.memberService.getMember(id);
   }
 
   // TODO: add authentication guard for admin
+  @UseGuards(JwtAuthGuard)
   @Delete('/:id')
   deleteMember(@Param('id') id) {
     return this.memberService.deleteMember(id);
