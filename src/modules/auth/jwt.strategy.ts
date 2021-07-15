54564fa70daf0d838020d9dc0172cabfcf4e70ed
Martijn Faber <martijn@fabtronics.nl>
2021-07-15 14:57:51 +0200

    fix(auth): change token to secret

--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -9,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>('jwt.token'),
+      secretOrKey: configService.get<string>('jwt.secret'),
     });
   }
 
