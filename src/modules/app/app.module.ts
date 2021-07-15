2077047032dbcc6dee50866b6433b97f093066b7
Martijn Faber <martijn@fabtronics.nl>
2021-07-15 12:09:22 +0200

    fix(app): add prisma module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import config from 'config';
+import { PrismaModule } from '@/modules/prisma/prisma.module';
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import config from 'config';
       isGlobal: true,
       load: [config],
     }),
+    PrismaModule,
   ],
 })
 export class AppModule {}
