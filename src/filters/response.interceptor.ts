10138212aae5275a61ee405e9fb0809039d00a90
Martijn Faber <martijn@fabtronics.nl>
2021-07-15 10:53:56 +0200

    feat(main): add response interceptor

---
+++ src/filters/response.interceptor.ts
@@ -0,0 +1,33 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import dayjs from 'dayjs';
+
+export interface Response<T> {
+  code: number;
+  data: T;
+}
+
+@Injectable()
+export class ResponseInterceptor<T> implements NestInterceptor<T, Response<T>> {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<Response<T>> {
+    const { statusCode } = context.switchToHttp().getResponse();
+
+    return next.handle().pipe(
+      map((data) => ({
+        timestamp: dayjs().format(),
+        code: statusCode,
+        data,
+      })),
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/modules/app/app.module';
 import { ConfigService } from '@nestjs/config';
 import { HttpExceptionFilter } from '@/filters/http-exception.filter';
+import { ResponseInterceptor } from '@/filters/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,7 @@ async function bootstrap() {
 
   app.enableCors();
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new ResponseInterceptor());
   app.setGlobalPrefix(config.get('prefix'));
   await app.listen(config.get('port'));
 }
